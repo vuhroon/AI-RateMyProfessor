8850691a7059c665616da83a45416cd7bf47f303
vuhroon <VarunSinghX97@gmail.com>
2024-09-19 12:45:12 -0400

    push

--- app/rmp/page.js
+++ app/rmp/page.js
@@ -1,5 +1,4 @@
 "use client";
-// import { Pinecone } from '@pinecone-database/pinecone';
 import reviews from "../../reviews.json";
 import { Box, FileX } from "lucide-react";
 import { Button } from "@/components/ui/button";
